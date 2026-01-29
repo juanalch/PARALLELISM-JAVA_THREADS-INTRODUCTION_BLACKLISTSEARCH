536164e93c264d7341bfbe80a225e67129ddbf79
juanalch <lozanochavesjuana@gmail.com>
2026-01-29 17:19:34 -0500

    feat(HostBlackList):Update HostBlackListSearchThread

--- src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListSearchThread.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListSearchThread.java
@@ -1,7 +1,10 @@
 package edu.eci.arsw.blacklistvalidator;
 
-import edu.eci.arsw.spamkeywordsdatasource.*;
-import java.util.*;
+import edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Thread that performs parallel search of an IP address in a segment
@@ -18,14 +21,22 @@ import java.util.*;
  * @author Anderson Fabian Garcia Nieto
  * @author Juana Lozano Chaves
  */
+
+
 public class HostBlackListSearchThread extends Thread {
 
     private final int startIndex;
     private final int endIndex;
     private final String ipaddress;
     private final HostBlacklistsDataSourceFacade facade;
-    private final List<Integer> occurrences = new LinkedList<>();
     private int checkedLists = 0;
+    // new local occurrences list
+    private final List<Integer> occurrences = new LinkedList<>();
+
+    // new shared state
+    private final AtomicInteger globalOccurrences;
+    private final AtomicBoolean stopSearch;
+
 
     /**
      * Creates a new blacklist search thread.
@@ -35,12 +46,15 @@ public class HostBlackListSearchThread extends Thread {
      * @param ipaddress     IP address to validate (e.g., "200.24.34.55")
      * @param facade        instance of the blacklist data source facade
      */
-    public HostBlackListSearchThread(int startIndex, int endIndex, String ipaddress, 
-                                     HostBlacklistsDataSourceFacade facade) {
+
+    public HostBlackListSearchThread(int startIndex,int endIndex,String ipaddress,HostBlacklistsDataSourceFacade facade, AtomicInteger globalOccurrences, AtomicBoolean stopSearch) {
+
         this.startIndex = startIndex;
         this.endIndex = endIndex;
         this.ipaddress = ipaddress;
         this.facade = facade;
+        this.globalOccurrences = globalOccurrences;
+        this.stopSearch = stopSearch;
     }
 
     /**
@@ -54,12 +68,34 @@ public class HostBlackListSearchThread extends Thread {
      */
     @Override
     public void run() {
-        for (int i = startIndex; i < endIndex; i++) {
+
+        for (int i = startIndex; i < endIndex && !stopSearch.get(); i++) {
+
             checkedLists++;
+
             if (facade.isInBlackListServer(i, ipaddress)) {
                 occurrences.add(i);
+
+                // atomic incremet of the global count of occurrences
+                int total = globalOccurrences.incrementAndGet();
+
+                // If the limit is reached, stop the global search
+                if (total >= HostBlackListsValidator.BLACK_LIST_ALARM_COUNT) {
+                    stopSearch.set(true);
+                    break;
+                }
             }
         }
+    }
+    /**
+     * Returns the list of blacklist numbers where the IP address was found.
+     *
+     * @return List containing the indices of blacklists that contain the IP.
+     *         The list is empty if the IP was not found in any blacklist.
+     */
+
+    public List<Integer> getOccurrences() {
+        return occurrences;
     }
 
     /**
@@ -70,14 +106,5 @@ public class HostBlackListSearchThread extends Thread {
     public int getCheckedLists() {
         return checkedLists;
     }
-
-    /**
-     * Returns the list of blacklist numbers where the IP address was found.
-     *
-     * @return List containing the indices of blacklists that contain the IP.
-     *         The list is empty if the IP was not found in any blacklist.
-     */
-    public List<Integer> getOccurrences() {
-        return occurrences;
-    }
 }
+
